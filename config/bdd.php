bd79090cde8f0333a85421f8a8c8a38bc004e05b
ingeni <ingeni_killer@hotmail.com>
2019-11-16 09:54:22 +0100

    config

--- config/bdd.php
+++ config/bdd.php
@@ -1,6 +1,6 @@
 <?php
-	define('HOST', 'localhost', true);
-	define('USER', 'root', true);
-	define('PASSWD', '', true);
-	define('DATABASE', 'appli_conges', true);
+	define('HOST', 'localhost:3307', true);
+	define('USER', 'userlocal', true);
+	define('PASSWD', 'userlocal!1979', true);
+	define('DATABASE', 'appliconges', true);
 ?>
